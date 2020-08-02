a9050c9fbab67f72027356f309f8022c4bb15ab9
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-08-02 22:14:32 +1000

    Extract typedefs

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,37 +1,22 @@
 import merge from "lodash.merge"
-import { gql, makeExecutableSchema } from "apollo-server"
+import { makeExecutableSchema } from "apollo-server"
 import { Launch } from "./schema/launch"
 import { LaunchConnection } from "./schema/launchConnection"
 import { Mission } from "./schema/mission"
-import { Rocket } from "./schema/rocket"
 import { TripUpdateResponse } from "./schema/tripUpdateResponse"
+import { typeDefs } from "./schema/typedefs"
 import { User } from "./schema/user"
 import { resolvers } from "./resolvers"
 
-const BaseTypeDef = gql`
-  type Query
-
-  type Mutation {
-    login(email: String): String # login token
-  }
-`
-
 export const schema =
   makeExecutableSchema({
-    typeDefs: [
-      BaseTypeDef,
-      Launch.typeDefs,
-      LaunchConnection.typeDefs,
-      Mission.typeDefs,
-      Rocket.typeDefs,
-      TripUpdateResponse.typeDefs,
-      User.typeDefs
-    ],
+    typeDefs,
     resolvers: merge(
       resolvers,
       Launch.resolvers,
       LaunchConnection.resolvers,
       Mission.resolvers,
-      TripUpdateResponse.resolvers
+      TripUpdateResponse.resolvers,
+      User.resolvers
     )
   })

01b1b1d943233c1b4808f5f7c42129ffc941f8f8
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-08-02 22:25:52 +1000

    Add type to schema

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,8 +1,9 @@
+import { GraphQLSchema } from "graphql"
 import { makeExecutableSchema } from "apollo-server"
 import { resolvers } from "./schema/resolvers"
 import { typeDefs } from "./schema/typedefs"
 
-export const schema =
+export const schema: GraphQLSchema =
   makeExecutableSchema({
     typeDefs,
     resolvers
